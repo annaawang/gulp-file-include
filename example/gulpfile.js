375934b6c161a03e7d72f7743eb0cece14dd5bd3
tsi <tsi.shlidor@gmail.com>
2014-07-09 09:48:20 +0300

    Compile inner pages

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,13 +1,22 @@
 'use strict';
 
 var gulp = require('gulp'),
-  fileinclude = require('../index');
+    fileinclude = require('gulp-file-include');
 
 gulp.task('include', function() {
+
   gulp.src(['index.html'])
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@file'
     }))
     .pipe(gulp.dest('./result'));
+
+  gulp.src(['pages/page.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('./result/pages'));
+
 });
